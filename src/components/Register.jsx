2197ddd4a96cad33e02074c9fe2678a018b13475
dbaudetm <92870616+dbaudetm@users.noreply.github.com>
2022-06-02 19:03:59 +0200

    subida de archivos3

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -58,7 +58,8 @@ const Register = () => {
 
 
             //${import.meta.env.VITE_backendUrl}
-            const res = await axios.post(`https://tfmevaluatefilm.herokuapp.com/api/usuarios/register`, { nombreUsuario, passwordUsuario, emailUsuario })
+            console.log(import.meta.env.VITE_backendUrl)
+            const res = await axios.post(`${import.meta.env.VITE_backendUrl}/api/usuarios/register`, { nombreUsuario, passwordUsuario, emailUsuario })
             console.log(res.data)
 
             if (res.data.error != undefined) {
